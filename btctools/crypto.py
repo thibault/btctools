31aa4b451b5ef5e1656924535a10ef3873a5c4b7
Thibault Jouannic <thibault@miximum.fr>
2014-05-09 19:54:53 +0200

    Crypto utilities now use bytes

--- btctools/crypto.py
+++ btctools/crypto.py
@@ -6,48 +6,31 @@ import binascii
 from ripemd import RIPEMD160
 
 
-def hash160(string):
-    data = string.encode('utf-8')
+def hash160(data):
     return RIPEMD160(data).digest()
 
 
-def _sha256_bin(data):
+def sha256(data):
     """hashes a bytes array and return binary value."""
     digest = hashlib.sha256(data).digest()
     return digest
 
 
-def sha256(string):
-    """Hashes a string and return a bytes array."""
-    data = string.encode('utf-8')
-    hash = _sha256_bin(data)
+def double_sha256(data):
+    """Double hash a bytes array and return a binary value."""
+    hash = sha256(sha256(data))
     return hash
 
 
-def double_sha256(string):
-    """Double hash a string and return a bytes array."""
-    data = string.encode('utf-8')
-    hash = _sha256_bin(_sha256_bin(data))
-    return hash
-
-
-def sha256_hex(string):
+def sha256_hex(data):
     """Hashes a string and return a hex string."""
-    data = sha256(string)
+    data = sha256(data)
     hex = binascii.hexlify(data)
     return hex.decode('utf-8')
 
 
-def double_sha256_hex(string):
+def double_sha256_hex(data):
     """Double hashes a string and return a hex string."""
-    data = double_sha256(string)
+    data = double_sha256(data)
     hex = binascii.hexlify(data)
     return hex.decode('utf-8')
-
-
-def bytes_to_int(bytes):
-    """Converts a bytes array into a big number."""
-    hex_data = binascii.hexlify(bytes).encode('utf-8')
-    number = int(hex_data, 16)
-
-    return number
